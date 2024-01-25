f075a4f346aa3e946b7e3bdc118e27997f3b4543
chamil-dev <77248515+chamil-dev@users.noreply.github.com>
2024-01-25 16:50:26 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//aap.js
+
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, Docker!\n');
+    });
+
+    const PORT = 3000;
+    server.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+      });
+
